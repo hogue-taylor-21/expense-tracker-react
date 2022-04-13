ea611a537e98609112467ca08a6abe6cd47b1f7d
Tay <Tay@Taylors-MacBook-Pro.local>
2022-04-12 21:22:15 -0400

    initial commit

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/<front end app folder name>/build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
